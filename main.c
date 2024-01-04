9113e9af65610b335e07cb50a5c580b7f09b78db
Iaskasan <nokiesan@yahoo.fr>
2024-01-04 14:17:38 +0100

    added push function and started main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "monty.h"
+
+int main(void)
+{
+	stack_t *stack = NULL;
+
+	
+}
---
+++ push.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * push - opcode to push a value onto the stack
+ * @stack: double pointer to the beginning of the stack
+ * @value: value to be pushed onto the stack
+ */
+
+void push(stack_t **stack, int value)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: Unable to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+}
